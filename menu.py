594c633b0d29b5b73581cf67a181d139d04b69ee
JakevK <jakevankeulen@gmail.com>
2020-05-12 09:35:04 +1200

    h

--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
-import text, window, readchar
+import text, window, readchar, re
 
 
 
@@ -9,25 +9,28 @@ window_y = window_dimensions["y"]
 
 
 def multi_choice(title, options, width=0):
+
+    margin_top = (window_y - len(options) - 1) // 2 - 1
+    margin_bottom = window_y - len(options) - margin_top - 5
+
+    width = max(max(map(len, options)), width)
+
+
+    title_display = text.center(title, width)
+    title_display = text.decorate(
+        title_display, 
+        bold=1, 
+        color="blue", 
+        underline=1
+    )
+    title_display = text.center(title_display)
+
+
     selected = 0
 
     while True:
         window.clear()
 
-        margin_top = (window_y - len(options) - 1) // 2 - 1
-        margin_bottom = window_y - len(options) - margin_top - 5
-
-        width = max(max(map(len, options)), width)
-
-
-        title_display = text.center(title, width)
-        title_display = text.decorate(
-            title_display, 
-            bold=1, 
-            color="blue", 
-            underline=1
-        )
-        title_display = text.center(title_display)
 
         options_display = [option.ljust(width) for option in options]
         options_display[selected] = text.decorate(
@@ -65,5 +68,73 @@ def multi_choice(title, options, width=0):
             return options[selected]
 
 
-def text_input(message, validation):
-    pass
+
+def type_input(message, min_length, max_length, validation_func=None):
+    margin_top = (window_y - 3) // 2 - 1
+    margin_bottom = window_y - margin_top - 6
+
+    message_display = text.decorate(message, color="blue")
+    message_display = text.center(message_display)
+
+
+    chars = []
+    cursor_pos = 0
+    
+    while True:
+        window.clear()
+
+        
+        formatted_chars = [*chars, ' ']
+        formatted_chars[cursor_pos] = text.decorate(
+            formatted_chars[cursor_pos], 
+            reversed=1
+        )
+        formatted_chars = ' ' + ''.join(formatted_chars)
+        formatted_chars = text.center(
+            formatted_chars, 
+            max(
+                len(chars), 
+                max_length + 2
+            )
+        )
+        formatted_chars = text.decorate(formatted_chars, underline=1)
+        formatted_chars = text.center(formatted_chars)
+
+
+        print('\n' * margin_top)
+        print(message_display + '\n')
+        print(formatted_chars)
+        print('\n' * margin_bottom)
+
+
+        key_press = readchar.readkey()
+
+        if key_press == readchar.key.LEFT:
+            if cursor_pos:
+                cursor_pos -= 1
+        elif key_press == readchar.key.RIGHT:
+            if cursor_pos < len(chars):
+                cursor_pos += 1
+
+        elif key_press == readchar.key.BACKSPACE:
+            if cursor_pos:
+                chars.pop(cursor_pos - 1)
+                cursor_pos -= 1
+
+        elif key_press == readchar.key.ENTER:
+            return ''.join(chars)
+
+        elif len(repr(key_press)) == 3:
+            if len(chars) < max_length and validation_func(chars, key_press):
+                chars.insert(cursor_pos, key_press)
+                cursor_pos += 1
+
+
+def str_validation(chars, new_char):
+    return True
+
+def str_input(message, min_length, max_length):
+    return type_input(min_length, max_length)
+
+
+def int_input(message, min_value, max_value):
